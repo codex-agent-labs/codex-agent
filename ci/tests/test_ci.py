521d04062c5672d090150869f40aa234c6e771cb
ciurlaro <cesareiurlaro@gmail.com>
2026-08-24 01:39:52 +0200

    Classify protected Node release wiring

--- ci/tests/test_ci.py
+++ ci/tests/test_ci.py
@@ -614,9 +614,13 @@ class RealImpactPlanTest(unittest.TestCase):
             matching_lanes("test", gradle_tasks),
         )
 
-        registration = prefix + "ProtectedRuntimeCandidateRegistration.kt"
-        self.assertEqual(set(), matching_lanes("production", registration))
-        self.assertEqual({"contracts"}, matching_lanes("test", registration))
+        for filename in (
+            "ProtectedNodeRuntimeRegistration.kt",
+            "ProtectedRuntimeCandidateRegistration.kt",
+        ):
+            registration = prefix + filename
+            self.assertEqual(set(), matching_lanes("production", registration))
+            self.assertEqual({"contracts"}, matching_lanes("test", registration))
 
         future_source = prefix + "FutureProductionTask.kt"
         for category in ("production", "test", "metadata"):
